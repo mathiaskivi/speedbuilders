1e0299198bd9049df02ee39f387f18b08c7ecab1
Mathias Kivi <6755425+mathiaskivi@users.noreply.github.com>
2024-10-06 12:24:51 +0300

    Remove BungeeCord mode in preparation for new solution

--- src/main/java/ee/mathiaskivi/speedbuilders/SpeedBuilders.java
+++ src/main/java/ee/mathiaskivi/speedbuilders/SpeedBuilders.java
@@ -1,6 +1,5 @@
 package ee.mathiaskivi.speedbuilders;
 
-import ee.mathiaskivi.speedbuilders.bungeecord.BungeeCord;
 import ee.mathiaskivi.speedbuilders.multiworld.MultiWorld;
 import ee.mathiaskivi.speedbuilders.utility.Configuration;
 import ee.mathiaskivi.speedbuilders.utility.Statistics;
@@ -11,43 +10,36 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.sql.SQLException;
 
 public class SpeedBuilders extends JavaPlugin {
-
-	public BungeeCord bungeeCord;
 	public MultiWorld multiWorld;
 
 	public Configuration configuration;
 	public Statistics statistics;
 
 	public void onEnable() {
-		Bukkit.getLogger().info("[SpeedBuilders] SpeedBuilders v" + getDescription().getVersion() + " is enabled!");
-
 		new Metrics(this, 8034);
 
 		configuration = new Configuration();
 		statistics = new Statistics();
 
 		configuration.loadConfig("config.yml");
-		if (configuration.getConfig("config.yml").getString("plugin.version").equalsIgnoreCase("bungeecord")) {
-			bungeeCord = new BungeeCord();
-			bungeeCord.onEnable();
-		} else if (configuration.getConfig("config.yml").getString("plugin.version").equalsIgnoreCase("multiworld")) {
+		if (configuration.getConfig("config.yml").getString("plugin.version").equalsIgnoreCase("multiworld")) {
 			multiWorld = new MultiWorld();
 			multiWorld.onEnable();
 		} else {
-			Bukkit.getLogger().info("");
-			Bukkit.getLogger().severe("[SpeedBuilders] Version for SpeedBuilders plugin has not been set!");
-			Bukkit.getLogger().severe("[SpeedBuilders] Follow this instruction to fix this problem:");
-			Bukkit.getLogger().severe("[SpeedBuilders] Change plugin version to \"bungeecord\" or \"multiworld\" in config.yml file.");
-			Bukkit.getLogger().info("");
+			getLogger().info("");
+			getLogger().severe("[SpeedBuilders] Version for SpeedBuilders plugin has not been set!");
+			getLogger().severe("[SpeedBuilders] Follow this instruction to fix this problem:");
+			getLogger().severe("[SpeedBuilders] Change plugin version to \"multiworld\" in config.yml file.");
+			getLogger().info("");
 			Bukkit.getPluginManager().disablePlugin(this);
 		}
+
+		getLogger().info("[SpeedBuilders] SpeedBuilders v" + getDescription().getVersion() + " is enabled!");
 	}
 
 	public void onDisable() {
-		if (bungeeCord != null || multiWorld != null) {
-			if (configuration.getConfig("config.yml").getString("plugin.version").equalsIgnoreCase("bungeecord")) {
-				bungeeCord.onDisable();
-			} else if (configuration.getConfig("config.yml").getString("plugin.version").equalsIgnoreCase("multiworld")) {
+		if (multiWorld != null) {
+			if (configuration.getConfig("config.yml").getString("plugin.version").equalsIgnoreCase("multiworld")) {
 				multiWorld.onDisable();
 			}
 
@@ -58,11 +50,7 @@ public class SpeedBuilders extends JavaPlugin {
 			}
 		}
 
-		Bukkit.getLogger().info("[SpeedBuilders] SpeedBuilders v" + getDescription().getVersion() + " is disabled!");
-	}
-
-	public BungeeCord getBungeeCord() {
-		return bungeeCord;
+		getLogger().info("[SpeedBuilders] SpeedBuilders v" + getDescription().getVersion() + " is disabled!");
 	}
 
 	public MultiWorld getMultiWorld() {
